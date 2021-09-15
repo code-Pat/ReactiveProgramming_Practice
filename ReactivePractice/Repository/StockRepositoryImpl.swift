a559d36b20c3d6688328239450829ce4ccbd58dd
Donggeun Lee <patlee997@gmail.com>
2021-09-15 17:32:07 +0900

    repository setup

---
+++ ReactivePractice/Repository/StockRepositoryImpl.swift
@@ -0,0 +1,12 @@
+//
+//  StockRepositoryImpl.swift
+//  ReactivePractice
+//
+//  Created by Donggeun Lee on 2021/09/15.
+//
+
+class StockRepositoryImpl: StockRepository {
+    var apiKey: String = "JSHOR8KXBN10Y5EA"
+    
+}
+
