9c8fc5a3365503d1699d2a21eed9fa2e847f2934
Donggeun Lee <patlee997@gmail.com>
2021-09-15 16:51:43 +0900

    StockListController Setup

---
+++ ReactivePractice/Controller/StockListController.swift
@@ -0,0 +1,21 @@
+//
+//  StockListController.swift
+//  ReactivePractice
+//
+//  Created by Donggeun Lee on 2021/09/15.
+//
+
+import UIKit
+
+class StockListController: BaseViewController {
+    let selfView = StockListView()
+    
+    override func configureUI() {
+        view.addSubview(selfView)
+        selfView.translatesAutoresizingMaskIntoConstraints = false
+        selfView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        selfView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        selfView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        selfView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+}
