843b5f8ac157df9f34a8b3df1fe37f3654fc8c4f
Donggeun Lee <patlee997@gmail.com>
2021-09-28 13:32:33 +0900

    Potential error fixed, code style fixed, encoding improved.

--- ReactivePractice/Repository/StockRepositoryImpl.swift
+++ ReactivePractice/Repository/StockRepositoryImpl.swift
@@ -8,18 +8,74 @@ import Combine
 import Foundation
 
 class StockRepositoryImpl: StockRepository {
+    
+    enum StockRepositoryError: Error {
+        case encoding
+        case bad_url
+    }
+    
     let apiKey: String = "JSHOR8KXBN10Y5EA"
     let decoder = JSONDecoder()
     
     func fetchStockPublisher(keywords: String) -> AnyPublisher<StockResult, Error> {
-        let urlString = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(keywords)&apikey=\(apiKey)"
-        
-        guard let url = URL(string: urlString) else {
-            let error = URLError(.badURL)
+        let queryResult = parseQueryString(text: keywords)
+        var query = ""
+        switch queryResult {
+        case.failure(let error):
             return Fail(error: error).eraseToAnyPublisher()
+        case .success(let value):
+            query = value
         }
         
-        return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }.decode(type: StockResult.self, decoder: decoder).receive(on: RunLoop.main).eraseToAnyPublisher()
+        let urlResult = parseURL(urlString: "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(query)&apikey=\(apiKey)")
+        
+        switch urlResult {
+        case .failure(let error):
+            return Fail(error: error).eraseToAnyPublisher()
+        case .success(let url):
+            return URLSession.shared.dataTaskPublisher(for: url).map {$0.data}.decode(type: StockResult.self, decoder: decoder).receive(on: RunLoop.main).eraseToAnyPublisher()
+            
+        }
+      
+    }
+    
+    func fetchTimeSeriesPublisher(keywords: String) -> AnyPublisher<TimeSeriesMonthlyAdjusted, Error> {
+        let queryResult = parseQueryString(text: keywords)
+        var query = ""
+        switch queryResult {
+        case.failure(let error):
+            return Fail(error: error).eraseToAnyPublisher()
+        case .success(let value):
+            query = value
+        }
+        
+        let urlResult = parseURL(urlString: "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=\(query)&apikey=\(apiKey)")
+        
+        switch urlResult {
+        case .failure(let error):
+            return Fail(error: error).eraseToAnyPublisher()
+        case .success(let url):
+            return URLSession.shared.dataTaskPublisher(for: url).map {$0.data}.decode(type: TimeSeriesMonthlyAdjusted.self, decoder: decoder).receive(on: RunLoop.main).eraseToAnyPublisher()
+            
+        }
+    }
+    
+    private func parseURL(urlString: String) -> Result<URL, Error> {
+        if let url = URL(string: urlString) {
+            return .success(url)
+        } else {
+            let error: StockRepositoryError = .bad_url
+            return .failure(error)
+        }
+    }
+    
+    private func parseQueryString(text: String) -> Result<String, Error> {
+        if let query = text.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
+            return .success(query)
+        } else {
+            let error: StockRepositoryError = .encoding
+            return .failure(error)
+        }
     }
 }
 
