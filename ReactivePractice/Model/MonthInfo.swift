7ac34172a5a3d51342a393a5bbbb7d3c8a111a5e
Donggeun Lee <patlee997@gmail.com>
2021-09-28 15:19:22 +0900

    MonthInfo file added

---
+++ ReactivePractice/Model/MonthInfo.swift
@@ -0,0 +1,15 @@
+//
+//  MonthInfo.swift
+//  ReactivePractice
+//
+//  Created by Donggeun Lee on 2021/09/28.
+//
+
+import Foundation
+
+struct MonthInfo {
+    var date: Date
+    var adjustedOpen: Double
+    var adjustedClose: Double
+    
+}
