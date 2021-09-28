530a298b060d2652297108ed2bcd07e70120376d
Donggeun Lee <patlee997@gmail.com>
2021-09-28 14:34:23 +0900

    StockDetailController edited

--- ReactivePractice/Controller/StockDetailController.swift
+++ ReactivePractice/Controller/StockDetailController.swift
@@ -7,18 +7,23 @@
 
 import UIKit
 import Pure
+import RxCocoa
+import WebKit
 
 class StockDetailController: BaseViewController, FactoryModule {
     struct Dependency {
         let stock: Stock
+        let viewModel: StockDetailViewModel
     }
     
     let selfView = StockDetailView()
+    let viewModel: StockDetailViewModel
     
     let stock: Stock
     
     required init(dependency: Dependency, payload: ()) {
         stock = dependency.stock
+        viewModel = dependency.viewModel
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -34,6 +39,12 @@ class StockDetailController: BaseViewController, FactoryModule {
         removeListners()
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel.viewDidLoad(symbol: stock.symbol ?? "")
+        bind()
+    }
+    
     override func configureUI() {
         view.backgroundColor = .systemBackground
         title = "Detail"
@@ -43,5 +54,21 @@ class StockDetailController: BaseViewController, FactoryModule {
         selfView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         selfView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         selfView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    func bind() {
+        viewModel.$timeSeriesMonthlyAdjusted.sink { TimeSeriesMonthlyAdjusted in
+            guard let timeSeriesMonthlyAdjusted = TimeSeriesMonthlyAdjusted else { return }
+            print("timeSeriesMonthlyAdjusted: \(timeSeriesMonthlyAdjusted)")
+        }.store(in: &subscriber)
+        
+        viewModel.$errorMessage.sink { errorMessage in
+            guard let errorMessage = errorMessage else { return }
+            print("errorMessage: \(errorMessage)")
+        }.store(in: &subscriber)
+        
+        viewModel.$loading.sink { loading in
+            self.selfView.loadingView.isHidden = !loading
+        }.store(in: &subscriber)
     }
 }
