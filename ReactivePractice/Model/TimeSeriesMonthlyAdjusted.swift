da865d387bf07cf7c593cf93666b1222864614c4
Donggeun Lee <patlee997@gmail.com>
2021-09-28 15:19:57 +0900

    edited - generating MonthInfos to organize date formats uniformly when receiving data

--- ReactivePractice/Model/TimeSeriesMonthlyAdjusted.swift
+++ ReactivePractice/Model/TimeSeriesMonthlyAdjusted.swift
@@ -36,4 +36,28 @@ struct TimeSeriesMonthlyAdjusted: Decodable {
             case symbol = "2. Symbol"
         }
     }
+    
+    func generateMonthInfos() -> [MonthInfo] {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        var monthInfos: [MonthInfo] = []
+        
+        let sortedSeries = series.sorted { $0.key > $1.key }
+        
+        sortedSeries.forEach { (dateString, ohlc) in
+            if let date = dateFormatter.date(from: dateString), let adjustedClose = Double(ohlc.adjustedClose), let adjustedOpen = generateAdjustedOpen(ohlc: ohlc) {
+                let monthInfo: MonthInfo = .init(date: date, adjustedOpen: adjustedOpen, adjustedClose: adjustedClose)
+                monthInfos.append(monthInfo)
+            }
+        }
+        return monthInfos
+    }
+    
+    private func generateAdjustedOpen(ohlc: OHLC) -> Double? {
+        var adjustedOpen: Double?
+        if let open = Double(ohlc.open), let close = Double(ohlc.close), let adjustedClose = Double(ohlc.adjustedClose) {
+            adjustedOpen = open * (adjustedClose / close)
+        }
+        return adjustedOpen
+    }
 }
