fb2d174847ae7ca26b4a07b81b33036da1d7d3b0
Donggeun Lee <patlee997@gmail.com>
2021-09-28 15:19:09 +0900

    edited using MonthInfo to organize date formats

--- ReactivePractice/Controller/StockDetailViewModel.swift
+++ ReactivePractice/Controller/StockDetailViewModel.swift
@@ -12,11 +12,14 @@ class StockDetailViewModel: BaseViewModel {
     @Published var loading = false
     @Published var errorMessage: String?
     @Published var timeSeriesMonthlyAdjusted: TimeSeriesMonthlyAdjusted?
+    @Published var monthInfos: [MonthInfo] = []
     
     let usecase: StockDetailUseCase
     
     func viewDidLoad(symbol: String) {
+        loading = true
         usecase.fetchTimeSeriesPublisher(keywords: symbol).sink { completion in
+            self.loading = false
             switch completion {
             case .failure(let error):
                 self.errorMessage = error.localizedDescription
@@ -30,5 +33,13 @@ class StockDetailViewModel: BaseViewModel {
     init(usecase: StockDetailUseCase) {
         self.usecase = usecase
         super.init()
+        bind()
+    }
+    
+    func bind() {
+        $timeSeriesMonthlyAdjusted.sink { timeSeriesMonthlyAdjusted in
+            guard let timeSeriesMonthlyAdjusted = timeSeriesMonthlyAdjusted else { return }
+            self.monthInfos = timeSeriesMonthlyAdjusted.generateMonthInfos()
+        }.store(in: &subscriber)
     }
 }
