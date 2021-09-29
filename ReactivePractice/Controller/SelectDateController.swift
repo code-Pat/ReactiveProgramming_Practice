63737bd6231be30a89aa99f0963ce1d8756e9b06
Donggeun Lee <patlee997@gmail.com>
2021-09-29 13:34:36 +0900

    SelectDateController file added

---
+++ ReactivePractice/Controller/SelectDateController.swift
@@ -0,0 +1,14 @@
+//
+//  SelectDateController.swift
+//  ReactivePractice
+//
+//  Created by Donggeun Lee on 2021/09/29.
+//
+
+import UIKit
+
+class SelectDateController: UITableViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+    }
+}
